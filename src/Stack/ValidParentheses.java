0b152b11ec8ba335015fb2d2923ddab785c013f0
Thomas <t.dluhosch@gmail.com>
2023-11-23 00:27:21 +0100

    added time complexity in validParantheses

--- src/Stack/ValidParentheses.java
+++ src/Stack/ValidParentheses.java
@@ -8,7 +8,7 @@ import java.util.Stack;
     - Open brackets must be closed by the same type of brackets.
     - Open brackets must be closed in the correct order.
     - Every close bracket has a corresponding open bracket of the same type.
-    -> time complexity: O()
+    -> time complexity: O(n)
  */
 
 public class ValidParentheses {
