87c3574dfe79935b2ecd1a86b0792b92d533ef72
Thomas <t.dluhosch@gmail.com>
2023-12-07 00:29:15 +0100

    solved TopKFrequentElements Problem

--- src/Arrays_and_Hashing/TopKFrequentElements.java
+++ src/Arrays_and_Hashing/TopKFrequentElements.java
@@ -1,5 +1,6 @@
 package Arrays_and_Hashing;
-import java.util.HashMap;
+
+import java.util.*;
 
 /*
     LeetCode Problem 347
@@ -11,21 +12,35 @@ public class TopKFrequentElements {
 
     public static int[] topKFrequent(int[] nums, int k) {
 
+        int length = nums.length;
+        int numberOfValuesK = 0;
+        int[] result = new int[k];
+        // map = number, frequency
         HashMap<Integer, Integer> map = new HashMap<>();
-        int[] ret = new int[10];
-        int index = 0;
+        Stack<Integer> stack = new Stack<>();
 
-        for (int i = 0; i<nums.length; i++){
-            if(map.containsKey(nums[i])){
-                map.put(nums[i], map.get(nums[i]) +1);
-            } else{
-                map.put(nums[i], 1);
+
+
+        for (int i = 0; i < length; i++) {
+
+            if (map.containsKey(nums[i])) {
+                map.put(nums[i], map.get(nums[i]) + 1);
+                continue;
             }
+            map.put(nums[i], 1);
         }
 
+        PriorityQueue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>(
+                (a, b) -> Integer.compare(b.getValue(), a.getValue())
+        );
 
+        pq.addAll(map.entrySet());
 
-        return ret;
+        // Add the top k frequent elements to the result array
+        for (int i = 0; i < k; i++) {
+            result[i] = pq.poll().getKey();
+        }
 
+        return result;
     }
 }
--- src/Arrays_and_Hashing/TopKFrequentElementsTest.java
+++ src/Arrays_and_Hashing/TopKFrequentElementsTest.java
@@ -1,6 +1,8 @@
 package Arrays_and_Hashing;
 
 import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertArrayEquals;
 
 public class TopKFrequentElementsTest {
@@ -13,6 +15,25 @@ public class TopKFrequentElementsTest {
         int[] actual = TopKFrequentElements.topKFrequent(input, k);
         assertArrayEquals(expected, actual);
     }
+
+    @Test
+    public void case_2(){
+        int[] input = {1};
+        int k = 1;
+        int[] expected = {1};
+        int[] actual = TopKFrequentElements.topKFrequent(input, k);
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void case_3(){
+        int[] input = {1,2};
+        int k = 2;
+        int[] expected = {1,2};
+        int[] actual = TopKFrequentElements.topKFrequent(input, k);
+        assertArrayEquals(expected, actual);
+    }
+
 
 
 }
